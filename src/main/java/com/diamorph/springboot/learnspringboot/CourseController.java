d4434a657a99d2b5cc3f6ec6d0f5ceed7fc08262
Vlad Tymoshenko <diamorph1309@gmail.com>
2023-07-08 13:29:35 -0400

    Initial commit

---
+++ src/main/java/com/diamorph/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,18 @@
+package com.diamorph.springboot.learnspringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(
+                new Course(1L, "Learn Spring Boot", "Vladyslav Tymoshenko"),
+                new Course(2L, "Learn Angular", "Peter Jackson")
+        );
+    }
+}
